128567c502486a1eec4f8d8ac3bbad2bf3496faa
BOA_Eng <boagds@gmail.com>
2026-04-28 13:04:16 +0100

    Day 3: added mul, pow and relu to engine.py

--- engine.py
+++ engine.py
@@ -4,6 +4,47 @@ class Value:
         self.data = data
         self.grad = 0
         self._backward = lambda:    None
-        self._prev = (_children)
+        self._prev = set(_children)
         self._op = _op 
    
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+        out._backward = _backward    
+
+        return out
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+
+        return out
+    
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)),   "only supporting int/float numbers"
+        out = Value(self.data**other, (self,), f'**{other}')
+
+        def _backward():
+            self.grad += other * self.data**(other-1) 
+        out._backward = _backward
+
+        return out   
+    
+    def relu(self):
+        out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU') 
+
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
+        out._backward = _backward
+
+        return out
+
